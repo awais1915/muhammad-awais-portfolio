ceb92c6f7f2475cbf74195f813e9191c8b1cbf88
Muhammad Awais <167126280+awais1915@users.noreply.github.com>
2026-07-05 15:48:33 +0500

    Change profile image format from PNG to JPEG

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "MUHAMMAD AWAIS",
-  profile: '/profile.png',
+  profile: '/profile.jpeg',
   designation: "Software Developer",
   description: "My name is Muhammad Awais, and I am a passionate and dedicated programmer. I have a strong self-learning attitude and adapt quickly to new technologies. I enjoy exploring innovative solutions and thrive on solving challenging problems. Web application development excites me, and I am comfortable working with almost all stacks.  and I enjoy building projects and solving problems using it. I am open to any job opportunities that align with my skills and interests.",
   email: 'awaismalik1915@gmail.com',
